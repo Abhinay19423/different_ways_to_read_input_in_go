f364ceaca9e98028090f1f618c04040dd2e75b15
Abhinay19423 <148855595+Abhinay19423@users.noreply.github.com>
2024-10-07 10:41:47 +0530

    Create readDiffInputs.go
    To read a input as array effieciently and easily

---
+++ readDiffInputs.go
@@ -0,0 +1,58 @@
+package main
+ 
+import (
+    "bufio"
+    "fmt"        
+    "strconv"
+    "strings"
+    "os"
+)
+
+func main() {    
+  in := bufio.NewReader(os.Stdin)
+  fmt.Println("enter the values")	
+  val := readInt(in)
+	// fmt.Println(reflect.TypeOf(val))
+  arr := make([]int, val)
+	arr = readArrInt(in)	
+	
+	for _, val := range arr{
+		fmt.Println(val)
+	}       
+}
+
+func readInt(in *bufio.Reader) int {
+	nStr, _ := in.ReadString('\n')
+	nStr = strings.ReplaceAll(nStr, "\r", "")
+	nStr = strings.ReplaceAll(nStr, "\n", "")
+	n, _ := strconv.Atoi(nStr)
+	return n
+}
+
+func readLineNumbs(in *bufio.Reader) []string {
+	line, _ := in.ReadString('\n')
+	line = strings.ReplaceAll(line, "\r", "")
+	line = strings.ReplaceAll(line, "\n", "")
+	numbs := strings.Split(line, " ")
+	return numbs
+}
+
+func readArrInt(in *bufio.Reader) []int {
+	numbs := readLineNumbs(in)
+	arr := make([]int, len(numbs))
+	for i, n := range numbs {
+		val, _ := strconv.Atoi(n)
+		arr[i] = val
+	}
+	return arr
+}
+
+func readArrInt64(in *bufio.Reader) []int64 {
+	numbs := readLineNumbs(in)
+	arr := make([]int64, len(numbs))
+	for i, n := range numbs {
+		val, _ := strconv.ParseInt(n, 10, 64)
+		arr[i] = val
+	}
+	return arr
+}
